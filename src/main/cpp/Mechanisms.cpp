d1181686cbbef22ece937d8051a44441d1753925
Developer2 <12o.ninerobotics@gmail.com>
2020-01-23 18:48:15 -0600

    Mechanisms

--- src/main/cpp/Mechanisms.cpp
+++ src/main/cpp/Mechanisms.cpp
@@ -7,15 +7,16 @@ Mechanisms::Mechanisms()
 }
 
 void Mechanisms::Crawl() {
-
+    crawlMotor.Set(.4);
 }
 
 void Mechanisms::Lift() {
-
+    liftMotor1.Set(.5);
+    liftMotor2.Set(.5);
 }
 
 void Mechanisms::Shoot() {
-
+    shootMotor.Set(.8);
 }
 
 void Mechanisms::Spin() {
@@ -23,3 +24,11 @@ void Mechanisms::Spin() {
 }
 
 
+void Mechanisms::RunMechanisms() {
+    if (controller.GetBButton()) {
+        Lift();
+    }
+    if (controller.GetAButton()) {
+        Shoot();
+    }
+}
--- src/main/include/Mechanisms.h
+++ src/main/include/Mechanisms.h
@@ -2,15 +2,24 @@
 #include <frc/XBoxController.h>
 #include <frc/SpeedControllerGroup.h>
 #include <frc/WPILib.h>
+#include <frc/XboxController.h>
 
 class Mechanisms {
     public:
     Mechanisms();
     private:
     void Crawl();
+    void Intake();
     void Lift();
     void Shoot();
     void Spin();
-    frc::VictorSP motor{0};
-    std::string x;
+    void RunMechanisms();
+    frc::XboxController controller{0};
+    frc::VictorSP crawlMotor{0};
+    frc::VictorSP liftMotor1{2};
+    frc::VictorSP liftMotor2{3};
+    frc::VictorSP shootMotor{4};
+    frc::VictorSP spinMotor{5};
+frc::PWMSpeedController
+//    std::string x;
 };
